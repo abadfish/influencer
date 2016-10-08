46f8e126e90637594d75c8613c4a5babf4b57671
abadfish <kirsten.ofarrell@gmail.com>
2016-10-08 16:31:28 -0400

    fixed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations', :omniauth_callbacks => "users/omniauth_callbacks"}
 
-<<<<<<< HEAD
-=======
   resource :industry
 
   resource :users, only: [:index,:show, :create, :edit, :update]
   get '/users', to: 'users#index'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
->>>>>>> origin/front-end
-
-  resource :industry
-
-  resource :users, only: [:index, :show, :create, :edit, :update]
-
-
 
 
 end
