37660a8c7e0552cfbb860a444698181e15fff88d
abadfish <kirsten.ofarrell@gmail.com>
2016-10-08 15:49:33 -0400

    broken log out path

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-    end      
+    end
   end
 
-       
+
 end
